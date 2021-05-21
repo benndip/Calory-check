ab835f1341ea35972ec6efbcc1fb44d2cfd65234
PeterKampete <peterkampete01@gmail.com>
2021-05-21 06:59:42 +0100

    did work on stats page

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,25 +1,29 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import deploymentConfig from '../Deployment/deploymentConfig';
-import logo from '../resources/favicon.png';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import deploymentConfig from "../Deployment/deploymentConfig";
+import logo from "../resources/favicon.png";
 
 class Footer extends Component {
-  render() {
-    return (
-      <div className="Footer container">
-        <img src={logo} alt="EasyCal" />
-        <p>&copy; EasyCal 2018</p>
+	render() {
+		return (
+			<div className="Footer container">
+				<img src={logo} alt="EasyCal" />
+				<p>&copy;Nutriment Fact 2021</p>
 
-        {!this.props.onLogin && <span className="Footer__links">
-        	<Link to={deploymentConfig().baseUrl + "/"}>Today's Log</Link><span className="Footer__link-separator">·</span>
-        	<Link to={deploymentConfig().baseUrl + "/stats"}>Stats</Link><span className="Footer__link-separator">·</span>
-        	<Link to={deploymentConfig().baseUrl + "/me"}>My Account</Link>
-        </span>}
+				{!this.props.onLogin && (
+					<span className="Footer__links">
+						<Link to={deploymentConfig().baseUrl + "/"}>Today's Log</Link>
+						<span className="Footer__link-separator">·</span>
+						<Link to={deploymentConfig().baseUrl + "/stats"}>Stats</Link>
+						<span className="Footer__link-separator">·</span>
+						<Link to={deploymentConfig().baseUrl + "/me"}>My Account</Link>
+					</span>
+				)}
 
-        <div className="clearfix"></div>
-      </div>
-    );
-  }
+				<div className="clearfix"></div>
+			</div>
+		);
+	}
 }
 
 export default Footer;
